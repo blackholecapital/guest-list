9ce0480626e6ccba6416be43c01aae38292ac762
factory-admin <info@blackholecapital.xyz>
2026-08-06 12:49:53 -0400

    Create PromoterControlPage component

---
+++ src/pages/PromoterControlPage.tsx
@@ -0,0 +1,5 @@
+import { useEffect, useState } from "react";
+import Shell from "../components/Shell";
+import { api } from "../api/client";
+
+export default function PromoterControlPage({ promoterSlug }
